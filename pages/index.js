45a32d40c598dc32278bee1905b954d6fc699ca4
Vadim <_s_e_t__@ukr.net>
2024-06-29 16:49:37 +0300

    added some code

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,52 @@
-function HomePage(props) {
+import fs from "fs/promises";
+
+import path from "path";
+
+const HomePage = (props) => {
+  const {products} = props
+  
+
+  if (!products) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+        </li>
+      ))}
     </ul>
   );
+};
+
+export async function getStaticProps() {
+  console.log('revalidate')
+  const filePath = path.join(process.cwd(), 'data','dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData)
+
+  if (!data) {
+    return {
+      redirect: {
+        destination:'/no-data'
+      }
+    }
+  }
+
+  if (data.products.length === 0) {
+    return {notFound:true}
+  }
+
+  return {
+    props: {
+      products:data.products
+    },
+    revalidate: 10,
+    
+  };
 }
 
 export default HomePage;
